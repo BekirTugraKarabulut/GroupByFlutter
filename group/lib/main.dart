4b294c2ac25c74f27a527eb90028aff594a2ca43
Hüseyin Çoban <129303709+HuseyinCoban7@users.noreply.github.com>
2025-12-26 11:23:05 +0300

    Update main.dart

--- group/lib/main.dart
+++ group/lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-import 'data/models/book.dart';
-import 'screens/book_list_screen.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Book Manager',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: const BookListScreen(),
-    );
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ReadingTrackerApp());
 }
